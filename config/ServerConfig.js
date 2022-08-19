ff2a71d19dd78d7693e4c04eb0ca4ef1a579e469
Daniel-Dev23 <danieldev.info@gmail.com>
2022-08-18 22:43:56 -0700

    feat: new functionalities were added:
    - Middleware configuration.
    - Router configuration.
    - Creation of "pathServices".
    - Configuration of "error-handlers" (error404 and error 500).

--- config/ServerConfig.js
+++ config/ServerConfig.js
@@ -1,4 +1,9 @@
+const cors    = require('cors');
 const express = require('express');
+const helmet  = require('helmet');
+const morgan  = require('morgan');
+
+const { error404, error500 } = require('../src/v1/middlewares/error-handler');
 
 class ServerConfig {
 
@@ -6,6 +11,34 @@ class ServerConfig {
 
         this.app = express();
         this.port = process.env.PORT || 3000;
+
+        this.pathServices = {
+            helloWorld: '/'
+        }
+
+        this.middlewares();
+        this.routers();
+
+    }
+
+    middlewares() {
+
+        this.app.use( cors() );
+        this.app.use( express.json() );
+        this.app.use( morgan('dev') );
+        this.app.use( helmet() );
+
+    }
+
+    routers() {
+
+        // Paths Services
+        this.app.use( this.pathServices.helloWorld, require('../src/v1/routers/HelloWorldRouter') );
+
+        // Error 404
+        this.app.use( error404 );
+        // Error 500
+        this.app.use( error500 );
 
     }
 
